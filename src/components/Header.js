c56ac222fb8870f442fa1c333f6e5ddd17e1811a
michaelokpala <micmictochukwu@gmail.com>
2023-03-10 09:39:17 +0100

    second commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
       <div className='flex justify-between items-center'>
         {/** logo*/}
         <a href='#'>
-          <img src={Logo} height='90' alt='' />
+          <img src={Logo} height={90} alt='' />
         </a>
         {/***button */}
         <button className='btn btn-sm'>Work with me</button>
